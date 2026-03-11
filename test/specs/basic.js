d87fa40123656fd727856bc12c0fbf88ff8e34d7
Ganesh <ganeshpradhan1996@gmail.com>
2026-03-11 15:42:51 +0530

    updated project for reporting

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,8 +1,16 @@
+import allureReporter from '@wdio/allure-reporter'
 console.log("hello world")
 describe('My first test', () => {
     it('should have the right title', () => {
         browser.url('https://www.google.com')
         const title = browser.getTitle()
+        allureReporter.addFeature('Login Feature')
+        allureReporter.addStory('Valid Login')
         // expect(title).toBe('Google')
+        allureReporter.addAttachment(
+    'Screenshot',
+    async () => await browser.takeScreenshot(),
+    'image/png'
+)
     })
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false
+        }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
