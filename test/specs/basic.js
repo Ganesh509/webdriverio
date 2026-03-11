6523b696ae1c506b1752f2280081bc215701f8bf
Ganesh509 <ganeshpradhan1996@gmail.com>
2026-03-11 13:24:08 +0530

    Updated basic.js

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+describe('My first test', () => {
+    it('should have the right title', () => {
+        browser.url('https://www.google.com')
+        const title = browser.getTitle()
+        // expect(title).toBe('Google')
+    })
+})
